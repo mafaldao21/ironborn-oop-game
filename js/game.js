1c0a6c641b156b5449d1e0a8099364c056888a39
mafaldao21 <mafalda.oliveira.21@icloud.com>
2022-04-07 15:28:35 +0100

    player and obstacle move

---
+++ js/game.js
@@ -0,0 +1,67 @@
+
+
+class Game {
+    constructor(create, draw){
+        this.player = null;
+        this.create = create;
+        this.draw = draw;
+    }
+
+    start(){
+        /*create and draw a player*/
+        this.player = new Player();
+        this.player.domElement = this.create("player"); //create a dom element with the class "player"
+        this.draw(this.player);
+
+        /*create and draw an obstacle*/
+        this.obstacle = new Obstacle;
+        this.obstacle.domElement = this.create("obstacle");
+        this.draw(this.obstacle);
+
+        // make obstacle move
+        setInterval( () => {
+        this.obstacle.moveDown();
+        this.draw(this.obstacle);
+        }, 50);
+    }
+
+    movePlayer(direction){
+        if(direction === "left"){
+            this.player.moveLeft();
+        } else if (direction === "right"){
+            this.player.moveRight();
+        }
+        this.draw(this.player);
+    }
+}
+
+
+class Player {
+    constructor() {
+        this.positionX = 50;
+        this.positionY = 0;
+        this.domElement = null;
+    }
+
+    moveLeft() {
+        this.positionX--;
+    }
+
+    moveRight() {
+        this.positionX++;
+    }
+}
+
+
+class Obstacle {
+    constructor() {
+        this.positionX = 50;
+        this.positionY = 100;
+        this.domElement = null
+    }
+
+    moveDown() {
+        this.positionY--;
+    }
+
+}
--- js/main.js
+++ js/main.js
@@ -1,29 +1,36 @@
 
 
-function createDomElement(className) {
+function createDomElement(className){
     const board = document.getElementById("board");
     const newElm = document.createElement("div");
     newElm.className = className;
 
-    board.appendChild(newElm)
+    board.appendChild(newElm);
 
     return newElm;
 }
 
-function drawDomElement(instance){
+
+function drawDomElement(instance){    
     instance.domElement.style.left = instance.positionX + "%";
     instance.domElement.style.bottom = instance.positionY + "%";
 }
 
+
+/* Start game */
 const game = new Game(createDomElement, drawDomElement);
-game.start()
+game.start();
 
+
+/* Event listeners */
 document.addEventListener("keydown", function(event){
-
-    if(event.key === "ArrowRight") {
-        game.movePlayer("right");
-    } else if(event.key === "ArrowLeft") {
-        game.movePlayer("left");
+    switch(event.key){
+        case "ArrowRight":
+            game.movePlayer("right");
+            break;
+        case "ArrowLeft":
+            game.movePlayer("left");
+            break;
     }
 });
 
